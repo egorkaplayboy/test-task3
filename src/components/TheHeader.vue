a3326539400751ca7d5ed4b9551394cd374f7cf5
egorkaplayboy <bel04ka228@mail.ru>
2024-02-17 17:09:03 +0300

    feat: made header and welcome component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,142 @@
+<template>
+  <nav class="nav">
+    <div class="nav__logo">
+      <svg class="nav__logo-icon">
+        <use xlink:href="../assets/logo.svg#logo"></use>
+      </svg>
+    </div>
+    <ul class="nav__list">
+      <li
+        class="nav__list-item"
+        v-for="(link, index) in navLinks"
+        :key="link"
+        @click="handleNavItemClick(index)"
+      >
+        {{ link }}
+      </li>
+    </ul>
+    <div class="nav__socials" v-if="isPopupOpen">
+      <div class="nav__socials-wrapper">
+        <svg class="nav__socials-item">
+          <use xlink:href="../assets/svg_sprite/sprite.svg#vk"></use>
+        </svg>
+        <svg class="nav__socials-item">
+          <use xlink:href="../assets/svg_sprite/sprite.svg#ok"></use>
+        </svg>
+        <svg class="nav__socials-item">
+          <use xlink:href="../assets/svg_sprite/sprite.svg#facebook"></use>
+        </svg>
+        <svg class="nav__socials-item">
+          <use xlink:href="../assets/svg_sprite/sprite.svg#insta"></use>
+        </svg>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      navLinks: [
+        "О нас",
+        "Наши ценности",
+        "Вакансии",
+        "Начало карьеры",
+        "Контакты",
+        "Социальные сети",
+      ],
+      isPopupOpen: false,
+    };
+  },
+  methods: {
+    handleNavItemClick(index) {
+      if (index === this.navLinks.length - 1) {
+        // Проверяем, является ли элемент последним
+        this.isPopupOpen = !this.isPopupOpen; // Переключаем состояние попапа
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/_vars.scss";
+@mixin horizontal {
+  display: flex;
+  align-items: center;
+}
+.nav {
+  position: relative;
+  @include horizontal;
+  justify-content: space-between;
+  &__logo-icon {
+    width: 115px;
+    height: 25px;
+    cursor: pointer;
+  }
+  &__list {
+    @include horizontal;
+    list-style-type: none;
+  }
+  &__list-item {
+    position: relative;
+    font-size: 16px;
+    color: $Dark;
+    margin: 32px;
+    padding-bottom: 27px;
+    cursor: pointer;
+    @include f400;
+
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 0;
+      height: 2px;
+      background-color: $Orange;
+      transition: width 0.3s;
+    }
+
+    &:hover::after {
+      width: 100%;
+    }
+  }
+  &__list-item:last-child:hover::after {
+    width: 0;
+  }
+  &__list-item:last-child {
+    font-size: 16px;
+    color: $Blue;
+    margin: 30px;
+    cursor: pointer;
+    @include f400;
+    display: flex;
+    align-items: center;
+  }
+  &__socials {
+    padding: 15px;
+    box-shadow: 0px 17px 46px 0px #00000014;
+    position: absolute;
+    top: 100%;
+    right: 0;
+    background-color: white;
+    z-index: 10;
+  }
+  &__socials-wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+  &__socials-item {
+    width: 25px;
+    height: 20px;
+    cursor: pointer;
+    padding: 20px;
+    fill: $Orange;
+    border: 1px solid $Gray;
+  }
+  &__socials-item:hover {
+    fill: $MainBlue;
+  }
+}
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="welcome">
+    <div class="container">
+      <div class="welcome__inner">
+        <div class="welcome__info">
+          <div class="welcome__info-title">
+            Энергия твоего роста! – заряжаем твою карьеру
+          </div>
+          <div class="welcome__info-button button">
+            <button>Cтать частью команды</button>
+          </div>
+        </div>
+        <div class="welcome_image">
+          <img
+            src="../assets/img/welcomePeoples.png"
+            alt="peoples"
+            loading="lazy"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+@import "../assets/scss/_vars.scss";
+.welcome {
+  background-image: url(../assets/img/welcomeBg.webp);
+  background-position: left;
+  background-size: cover;
+  overflow: hidden;
+  margin-bottom: 76px;
+  &__inner {
+    display: flex;
+    height: 448px;
+    align-items: center;
+    align-content: space-between;
+    position: relative;
+  }
+  &__info {
+    width: 500px;
+  }
+
+  &__info-title {
+    @include f700;
+    font-size: 36px;
+    line-height: 42px;
+    color: #fff;
+    margin-bottom: 56px;
+  }
+
+  &__info-button {
+    button {
+      width: 254px;
+      height: 64px;
+      background-color: $Orange;
+      border: none;
+      outline: none;
+      cursor: pointer;
+      @include f700;
+      color: #fff;
+      font-size: 16px;
+      transition: 0.15s ease;
+    }
+    button:hover {
+      opacity: 0.85;
+    }
+  }
+
+  &_image {
+    align-self: flex-end;
+    position: absolute;
+    right: -90px;
+    bottom: 0px;
+    width: 678px;
+    height: 430px;
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
