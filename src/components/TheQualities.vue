ed6f02af59bf90d218f72eab4efc3f7392ca7b47
egorkaplayboy <bel04ka228@mail.ru>
2024-02-21 14:47:35 +0300

    feat: made media queries for qualities component

--- src/components/TheQualities.vue
+++ src/components/TheQualities.vue
@@ -89,9 +89,121 @@
       </li>
     </ul>
   </section>
+  <section class="qualities-table">
+    <swiper :slides-per-view="1" :space-between="50">
+      <swiper-slide>
+        <ul class="qualities__list-table">
+          <li class="qualities__item-table">
+            <div>
+              <svg class="qualities__item-img-table" style="width: 66px">
+                <use
+                  xlink:href="../assets/svg_sprite/sprite.svg#career_ladder"
+                ></use>
+              </svg>
+            </div>
+            <div class="qualities__item-desc-table">
+              <h3>Развитие</h3>
+              <span
+                >Общего центра обслуживания. Современные технологии, проекты,
+                рабочие группы</span
+              >
+            </div>
+          </li>
+          <li class="qualities__item-table">
+            <div>
+              <svg class="qualities__item-img-table" style="width: 66px">
+                <use xlink:href="../assets/svg_sprite/sprite.svg#study"></use>
+              </svg>
+            </div>
+            <div class="qualities__item-desc-table">
+              <h3>Обучение</h3>
+              <span
+                >Общего центра обслуживания. Современные технологии, проекты,
+                рабочие группы</span
+              >
+            </div>
+          </li>
+        </ul>
+      </swiper-slide>
+      <swiper-slide>
+        <ul class="qualities__list-table">
+          <li class="qualities__item-table">
+            <div>
+              <svg class="qualities__item-img-table" style="width: 68px">
+                <use xlink:href="../assets/svg_sprite/sprite.svg#blocks"></use>
+              </svg>
+            </div>
+            <div class="qualities__item-desc-table">
+              <h3>Ответственность</h3>
+              <span
+                >Общего центра обслуживания. Современные технологии, проекты,
+                рабочие группы</span
+              >
+            </div>
+          </li>
+          <li class="qualities__item-table">
+            <div>
+              <svg class="qualities__item-img-table" style="width: 93px">
+                <use xlink:href="../assets/svg_sprite/sprite.svg#dinamic"></use>
+              </svg>
+            </div>
+            <div class="qualities__item-desc-table">
+              <h3>Динамика</h3>
+              <span
+                >Общего центра обслуживания. Современные технологии, проекты,
+                рабочие группы</span
+              >
+            </div>
+          </li>
+        </ul>
+      </swiper-slide>
+      <swiper-slide>
+        <ul class="qualities__list-table">
+          <li class="qualities__item-table">
+            <div>
+              <svg class="qualities__item-img-table" style="width: 72px">
+                <use xlink:href="../assets/svg_sprite/sprite.svg#comfort"></use>
+              </svg>
+            </div>
+            <div class="qualities__item-desc-table">
+              <h3>Комфорт</h3>
+              <span
+                >Общего центра обслуживания. Современные технологии, проекты,
+                рабочие группы</span
+              >
+            </div>
+          </li>
+          <li class="qualities__item-table">
+            <div>
+              <svg class="qualities__item-img-table" style="width: 54px">
+                <use
+                  xlink:href="../assets/svg_sprite/sprite.svg#security"
+                ></use>
+              </svg>
+            </div>
+            <div class="qualities__item-desc-table">
+              <h3>Надежность</h3>
+              <span
+                >Общего центра обслуживания. Современные технологии, проекты,
+                рабочие группы</span
+              >
+            </div>
+          </li>
+        </ul>
+      </swiper-slide>
+    </swiper>
+  </section>
 </template>
 <script>
-export default {};
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/scss";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+};
 </script>
 <style lang="scss" scoped>
 @import "../assets/scss/_vars.scss";
@@ -155,6 +267,78 @@ export default {};
       text-align: center;
       transition: opacity 0.2s;
     }
+  }
+}
+
+// Media
+
+@media (min-width: 1201px) {
+  .qualities-table {
+    display: none;
+  }
+}
+@media (max-width: 1200px) {
+  .qualities {
+    display: none;
+  }
+  .qualities__list-table {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .qualities__item-table {
+    height: 345px;
+    width: 345px;
+    padding: 60px 24px;
+    border: 1px solid $Gray2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    &:hover {
+      border: none;
+      @include shadow;
+      background-color: #fff;
+    }
+    &:hover .qualities__item-desc-table span {
+      opacity: 1;
+    }
+    &:hover .qualities__item-img-table {
+      fill: $Orange;
+      stroke: $Orange;
+    }
+  }
+  .qualities__item-desc-table {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    h3 {
+      @include f400;
+      font-size: 20px;
+      line-height: 26px;
+      color: $Dark;
+      text-align: center;
+      width: 100%;
+    }
+
+    span {
+      @include f400;
+      color: $Stroke;
+      font-size: 16px;
+      margin-top: 10px;
+      opacity: 0;
+      max-height: 0;
+      text-align: center;
+      transition: opacity 0.2s;
+    }
+  }
+  .qualities__item-img-table {
+    height: 70px;
+    fill: $Dark;
+    stroke: $Dark;
+    margin-bottom: 44px;
   }
 }
 </style>
